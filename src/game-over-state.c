f7ad30796a80032c2dca0ae49f7ef1bfae26b04f
Wendel Scardua <wendelscardua@gmail.com>
2022-10-03 18:02:37 -0300

    Working scorecode

--- src/game-over-state.c
+++ src/game-over-state.c
@@ -12,6 +12,123 @@
 #include "music/soundtrack.h"
 #include "hex-writer.h"
 
+#pragma bss-name(push, "ZEROPAGE")
+unsigned long scorecode;
+#pragma bss-name(pop)
+
+void base64digit() {
+  if (temp < 10) {
+    temp = temp + 0x10;
+  } else if (temp < 36) {
+    temp = temp - 10 + 0x21;
+  } else if (temp < 62) {
+    temp = temp - 36 + 0x41;
+  } else if (temp == 62) {
+    temp = 0x1f; // ?
+  } else if (temp == 63) {
+    temp = 0x20; // @
+  } else {
+    temp = 0x01; // ! = bug
+  }
+}
+
+void scorecode_stuff() {
+  temp_int_1 = player_score;
+  temp_int_2 = rng_seed;
+
+  // first byte of each + checksum
+  scorecode = 0;
+  for(i = 0; i < 8; i++) {
+    scorecode <<= 1;
+    scorecode |= (temp_int_1 & 1);
+    scorecode <<= 1;
+    scorecode |= (temp_int_2 & 1);
+    scorecode <<= 1;
+    scorecode |= ((temp_int_1 ^ temp_int_2) & 1);
+    temp_int_1 >>= 1;
+    temp_int_2 >>= 1;
+  }
+
+  k = 11;
+
+  temp = 0;
+  while(scorecode >= 262144) {
+    scorecode -= 262144;
+    temp++;
+  }
+
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  temp = 0;
+  while(scorecode >= 4096) {
+    scorecode -= 4096;
+    temp++;
+  }
+
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  temp = 0;
+  while(scorecode >= 64) {
+    scorecode -= 64;
+    temp++;
+  }
+
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  temp = (unsigned char) (scorecode);
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  // second byte of each + checksum
+
+  scorecode = 0;
+  for(i = 0; i < 8; i++) {
+    scorecode <<= 1;
+    scorecode |= (temp_int_1 & 1);
+    scorecode <<= 1;
+    scorecode |= (temp_int_2 & 1);
+    scorecode <<= 1;
+    scorecode |= ((temp_int_1 ^ temp_int_2) & 1);
+    temp_int_1 >>= 1;
+    temp_int_2 >>= 1;
+  }
+
+  temp = 0;
+  while(scorecode >= 262144) {
+    scorecode -= 262144;
+    temp++;
+  }
+
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  temp = 0;
+  while(scorecode >= 4096) {
+    scorecode -= 4096;
+    temp++;
+  }
+
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  temp = 0;
+  while(scorecode >= 64) {
+    scorecode -= 64;
+    temp++;
+  }
+
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+  temp = (unsigned char) (scorecode);
+  base64digit();
+  one_vram_buffer(temp, NTADR_A(k++, 13));
+
+}
+
 void game_over_state_init() {
   oam_size(0);
 
@@ -30,13 +147,14 @@ void game_over_state_init() {
   pal_bg(gameplay_bg_palette);
   pal_spr(gameplay_spr_palette);
 
-
   // display seed
   WRITE_INT_AS_HEX(rng_seed, 15, 15, temp);
 
   flush_vram_update2();
 
   multi_vram_buffer_horz(player_decimal_score, 5, NTADR_A(16, 10));
+
+  scorecode_stuff();
 
   // TODO: fade in?
   ppu_on_all();
---
+++ tools/scorecode.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+def digit(d)
+  if d =~/[0-9]/
+    d.ord - '0'.ord
+  elsif d =~ /[A-Z]/
+    d.ord - 'A'.ord + 10
+  elsif d =~ /[a-z]/
+    d.ord - 'a'.ord + 36
+  elsif d == '?'
+    62
+  elsif d == '@'
+    63
+  else
+    raise "What is #{d}"
+  end
+end
+
+def decode(base64ish)
+  puts base64ish
+  (digit(base64ish[0]) * 262144 + digit(base64ish[1]) * 4096 + digit(base64ish[2]) * 64 + digit(base64ish[3])).tap do |v|
+    puts v
+  end
+end
+
+code = ARGV.shift
+
+n = decode(code[4...8])
+
+score = seed = extra = 0
+8.times {
+  extra <<= 1
+  extra |= (n&1)
+  n>>=1
+  seed <<=1
+  seed |= (n&1)
+  n>>=1
+  score <<= 1
+  score|= (n&1)
+  n>>=1
+}
+
+n = decode(code[0...4])
+8.times {
+  extra <<= 1
+  extra |= (n&1)
+  n>>=1
+  seed <<=1
+  seed |= (n&1)
+  n>>=1
+  score <<= 1
+  score|= (n&1)
+  n>>=1
+}
+
+puts "score = #{score}"
+puts "seed = #{seed.to_s(16)}"
+puts "ok" if (score ^ seed) == extra
